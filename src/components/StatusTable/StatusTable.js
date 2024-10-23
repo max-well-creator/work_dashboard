fd8bdd526e4e6d35d160f99f5e4dc328e2799371
jordandevaney <jordandevaney28@gmail.com>
2024-10-23 11:53:34 -0400

    fixed bug where background color did not show up

--- src/components/StatusTable/StatusTable.js
+++ src/components/StatusTable/StatusTable.js
@@ -11,10 +11,27 @@ const StatusTable = () => {
 		STATUS_HEADING,
 		ASTATUS_HEADING,
 		OSTATUS_HEADING,
-		NOTES_HEADING,
 		AEXPIRATION_HEADING,
 		OEXPIRATION_HEADING,
+		APPROVED_COLOR,
+		REJECTED_COLOR,
+		PENDING_COLOR,
 	} = dataHeaders;
+
+	// Helper function to get the correct CSS class based on the color
+	const getRowColorClass = (color) => {
+		if (color === APPROVED_COLOR) {
+			return styles.statusApproved;
+		}
+		if (color === REJECTED_COLOR) {
+			return styles.statusRejected;
+		}
+		if (color === PENDING_COLOR) {
+			return styles.statusPending;
+		}
+
+		return "";
+	};
 
 	return (
 		<div className={styles.tableContainer}>
@@ -31,17 +48,20 @@ const StatusTable = () => {
 					</tr>
 				</thead>
 				<tbody>
-					{statesRegData.map((row) => (
-						<tr key={row.id} style={{ backgroundColor: row.color }}>
-							<td>{row.id}</td>
-							<td>{row.state}</td>
-							<td>{row.license_statues}</td>
-							<td>{row.a_status}</td>
-							<td>{row.o_status}</td>
-							<td>{row.a_expiration}</td>
-							<td>{row.o_expiration}</td>
-						</tr>
-					))}
+					{statesRegData.map((row) => {
+						const rowColorClass = getRowColorClass(row.color);
+						return (
+							<tr key={row.id} className={rowColorClass}>
+								<td>{row.id}</td>
+								<td>{row.state}</td>
+								<td>{row.license_statues}</td>
+								<td>{row.a_status}</td>
+								<td>{row.o_status}</td>
+								<td>{row.a_expiration}</td>
+								<td>{row.o_expiration}</td>
+							</tr>
+						);
+					})}
 				</tbody>
 			</table>
 		</div>
