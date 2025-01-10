321049e622a5f16be37a42fca86f305ab2130ee1
jordandevaney <jordandevaney28@gmail.com>
2025-01-10 13:04:27 -0500

    update tonnage report date for states

--- src/data/stateFrequencies.js
+++ src/data/stateFrequencies.js
@@ -13,85 +13,118 @@ export const stateFrequencies = [
 		october_2024_tonnage: ".01",
 		november_2024_tonnage: ".01",
 		inspection_fee: 0.375,
+		due: "The 20th of every month",
+		last_completed: "Dec 13, 2024",
 	},
 	{
 		state: "New Mexico",
-		frequency: "Monthly",
+		frequency: "Quarterly",
+		notes: "",
 		how_to_submit:
 			'<a href="https://nmag.nmsu.edu/USAPlants/SecurityLogin.aspx" target="_blank" rel="noopener noreferrer">https://nmag.nmsu.edu</a>',
+		due: "Jan 31 / April 31 / July 31 / October 31",
+		last_completed: "Jan 6, 2025",
 	},
 	{
 		state: "Arizona",
 		frequency: "Quarterly",
+		requires_county_in_tonnage: "Yes",
+		notes:
+			"Dowload forms at <a href='https://agriculture.az.gov/download-forms'>Arizonia Agriculture Website/a>",
 		how_to_submit:
 			"Arizona Department of Agriculture 1802 W. Jackson St. #78 Phoenix, AZ 85007. Make check payable to Arizona Department of Agriculture.",
+		due: "Jan 31 / April 31/ July 31 / October 31",
+		last_completed: "1/9/25",
 	},
 	{
 		state: "Delaware",
-		frequency: "Quarterly",
+		frequency: "Semi-Annually",
+		notes: "",
 		how_to_submit:
 			'If no tonnage, email <a href="mailto:DDA_AgCompliance@delaware.gov">DDA_AgCompliance@delaware.gov</a>. If tonnage, pay via <a href="https://www.velocitypayment.com/client/delaware/agriculture/agriculture/index.htm" target="_blank" rel="noopener noreferrer">Velocity Payment</a> and mail tonnage report and payment receipt to <a href="mailto:DDA_AgCompliance@delaware.gov">DDA_AgCompliance@delaware.gov</a>.',
+		due: "Jan 31 / July 31",
+		last_completed: "Jan 2, 2025",
 	},
 	{
 		state: "Georgia",
 		frequency: "Quarterly",
+		notes: "",
 		how_to_submit:
 			'<a href="https://www.kellysolutions.com/ga/fert/tonnage/login.asp" target="_blank" rel="noopener noreferrer">https://www.kellysolutions.com</a>',
+		due: "Jan 15 / April 15 / July 15 / October 15,",
 	},
 	{
 		state: "Iowa",
 		frequency: "Twice a year",
 		requires_county_in_tonnage: "No",
+		notes: "",
 		how_to_submit:
 			'Renew tonnage at <a href="https://iowadeptag.my.site.com/s/login/?amp=&ec=302" target= "_blank">Online at Iowa Department of Agriculture.</a>',
 		point_of_contact_name: "Alan Keller",
 		point_of_contact_phone: "515-281-8597",
 		point_of_contact_email: "?",
+		due: "Jul 15 / Jan 15",
+		last_completed: "Jan 2, 2025",
 	},
 	{
 		state: "Indiana",
 		frequency: "Twice a year",
+		requires_county_in_tonnage: "Yes",
+		notes:
+			"Dowload forms at <a href='https://oisc.purdue.edu/fertilizer/forms.html'>Purdue Website/a>",
 		how_to_submit:
 			"Office of Indiana State Chemist Purdue University 175 S. University Street West Lafayette, IN 47907-206.",
+		due: "Jan 31 / July 31",
+		last_completed: "Jan 10, 2025",
 	},
 	{
 		state: "Pennsylvania",
 		frequency: "Twice a year",
+		requires_county_in_tonnage: "Yes",
+		notes: "",
 		how_to_submit:
 			'<a href="https://www.paplants.pa.gov/" target="_blank" rel="noopener noreferrer">https://www.paplants.pa.gov/</a>',
-	},
-	{
-		state: "New Mexico",
-		frequency: "Quarterly",
-		how_to_submit:
-			'<a href="mailto:tonnage@nmda.nmsu.edu">tonnage@nmda.nmsu.edu</a>.',
+		due: "Jan 31 / July 31",
+		last_completed: "Jan 10, 2025",
 	},
 	{
 		state: "Michigan",
 		frequency: "Yearly",
+		notes: "",
 		how_to_submit:
 			'<a href="https://www.michigan.gov/mdard/plant-pest/fertilizer/fertilizerinfo/reporting-fertilizer-tonnage" target="_blank" rel="noopener noreferrer">https://www.michigan.gov</a>',
+		due: "Jan 31",
+		last_completed: "Jan 7, 2024",
 	},
 	{
 		state: "Minnesota",
 		frequency: "Yearly",
+		notes: "",
 		how_to_submit:
 			"MINNESOTA DEPARTMENT OF AGRICULTURE \n Attn: Cashier \n 625 Robert Street North Saint Paul, MN 55155-2538.",
+		due: "July 31",
+		last_completed: "September 20, 2024",
 	},
 	{
 		state: "Vermont",
 		frequency: "Yearly",
+		notes: "",
 		how_to_submit:
 			'Email to <a href="mailto:agr.feedseedfert@vermont.gov" target="_blank" rel="noopener noreferrer">mailto:agr.feedseedfert@vermont.gov</a>',
 		point_of_contact_name: "Lisa Fantelli",
 		point_of_contact_phone: "802-828-5050",
 		point_of_contact_email: "Lisa.fantelli@vermont.gov",
+		due: "Jan 15",
+		last_completed: "Jan 2, 2025",
 	},
 	{
 		state: "Mississippi",
 		frequency: "Yearly",
+		notes: "",
 		how_to_submit:
 			'<a href="https://agnet.mdac.ms.gov/fflsp/account/login" target="_blank" rel="noopener noreferrer">https://agnet.mdac.ms.gov</a>',
+		due: "Jul 1",
+		last_completed: "Jul 1",
 	},
 	{
 		state: "Utah",
@@ -100,50 +133,81 @@ export const stateFrequencies = [
 			'<a href="https://webapp.ag.utah.gov/productRegistration/registrant.jsp" target="_blank" rel="noopener noreferrer">https://agnet.mdac.ms.gov</a>. Mail a check to: Utah Department of Agriculture and Food; Plant Industry Division4315 South 2700 West TSOB South Building, Floor 2 Taylorsville, UT 84129-2128',
 		notes:
 			"Cover sheet for payment for for AG-USA is in dropbox under Utah Tonnage Reports",
+		due: "Nov 15",
+		last_completed: "Nov 1, 2024",
 	},
 	{
 		state: "North Dakota",
 		frequency: "Yearly",
+		notes: "",
 		how_to_submit:
 			'<a href="https://www.kellysolutions.com/nd/FertDistribLicenses/tonnage/login.asp" target="_blank" rel="noopener noreferrer">https://www.kellysolutions.com</a>',
 		point_of_contact_name: "Lukas Wagner",
 		point_of_contact_phone: "701 328-4922",
 		point_of_contact_email: "bkiefel@nd.gov",
-	},
-	{
-		state: "Wisconsin",
-		frequency: "Yearly",
-		how_to_submit:
-			"Make check payable to: Wisconsin Department of Agriculture, Trade and Consumer Protection (DATCP). Mail all forms & check to: State of Wisconsin, DATCP, Box 93178, Milwaukee WI 53293-0178.",
+		due: "",
+		last_completed: "Jan 2, 2025",
 	},
 	{
 		state: "Florida",
-		frequency: "Monthly, Quarterly",
-		requires_county_in_tonnage_quarterly: "No",
-		requires_county_in_tonnage_monthly: "Yes",
+		frequency: "Quarterly",
+		notes:
+			"https://www.fdacs.gov/Agriculture-Industry/Fertilizer-Licensing-and-Tonnage-Reporting",
+		requires_county_in_tonnage: "No",
 		how_to_submit:
 			'<a href="https://lims.flaes.org/HomeNew.aspx?ReturnUrl=/" target="_blank" rel="noopener noreferrer">https://lims.flaes.org</a>',
+		due: "Jan 31 / April 31/ July 31 / October 31",
+		last_completed: "Jan 6, 2024",
+	},
+	{
+		state: "Florida",
+		frequency: "Monthly",
+		notes:
+			"https://www.fdacs.gov/Agriculture-Industry/Fertilizer-Licensing-and-Tonnage-Reporting",
+		requires_county_in_tonnage: "Yes",
+		how_to_submit:
+			'<a href="https://lims.flaes.org/HomeNew.aspx?ReturnUrl=/" target="_blank" rel="noopener noreferrer">https://lims.flaes.org</a>',
+		due: "Every month on the 15th",
+		last_completed: "",
 	},
 	{
 		state: "California - SoilMaxx",
 		frequency: "Quaterly",
+		notes: "",
 		requires_county_in_tonnage_quarterly: "No",
 		how_to_submit:
-			'<a href="https://lims.flaes.org/HomeNew.aspx?ReturnUrl=/" target="_blank" rel="noopener noreferrer">https://lims.flaes.org</a>',
+			'<a href="https://inspect.cdfa.ca.gov/evj/ExtraView?killcache=-1515228896" target="_blank" rel="noopener noreferrer">https://lims.flaes.org</a>',
+		due: "Jan 31 / April 31 / July 31 / October 31",
+		last_completed: "Jan 7, 2025",
 	},
 	{
 		state: "California - SoilMaxx",
 		frequency: "Semi-Annually",
+		notes: "",
 		requires_county_in_tonnage: "Yes",
 		how_to_submit:
-			'<a href="https://lims.flaes.org/HomeNew.aspx?ReturnUrl=/" target="_blank" rel="noopener noreferrer">https://lims.flaes.org</a>',
+			'<a href="https://inspect.cdfa.ca.gov/evj/ExtraView?killcache=-1515228896" target="_blank" rel="noopener noreferrer">https://lims.flaes.org</a>',
+		due: "Jan 15 / Jul 15",
+		last_completed: "Jan 7, 2025",
+	},
+	{
+		state: "California - GroPal",
+		notes: "Always list no sales/ Report for two locations",
+		frequency: "Quarterly",
+		requires_county_in_tonnage: "No",
+		how_to_submit:
+			'<a href="https://inspect.cdfa.ca.gov/evj/ExtraView?killcache=-1515228896" target="_blank" rel="noopener noreferrer">https://lims.flaes.org</a>',
+		due: "Jan 31 / April 31 / July 31 / October 31",
+		last_completed: "Jan 7, 2025",
 	},
 	{
 		state: "California - GroPal",
 		notes: "Always list no sales",
-		frequency: "Quarterly",
-		requires_county_in_tonnage_quarterly: "No",
+		frequency: "Semi-Annually",
+		requires_county_in_tonnage: "No",
 		how_to_submit:
-			'<a href="https://lims.flaes.org/HomeNew.aspx?ReturnUrl=/" target="_blank" rel="noopener noreferrer">https://lims.flaes.org</a>',
+			'<a href="https://inspect.cdfa.ca.gov/evj/ExtraView?killcache=-1515228896" target="_blank" rel="noopener noreferrer">https://lims.flaes.org</a>',
+		due: "Jan 15 / Jul 15",
+		last_completed: "Jan 7, 2025",
 	},
 ];
